b7adfb853e99e17adece5f1960455332bf4f72c4
ZsZJ <ktj.lim@gmail.com>
2018-10-01 22:54:12 +0200

    Changed the local language to english & First implementation Role Authentication

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,12 @@ require('../css/global.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
+const $ = require('jquery');
+
+require('bootstrap');
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+})
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
